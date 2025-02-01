6396296d11bc5003b6d52afcdc624d62ce75f4a4
Marco <marco@parretto.com>
2025-02-01 15:57:06 +0100

    nieuwe functies toegevoegd

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Xml.Serialization;
 using gameslist;
 
 internal class Program
@@ -8,16 +9,22 @@ internal class Program
     private static void Main(string[] args)
     {
         var games_user = new List<string>();
+        string choice = null;
 
         while (true)
         {
             Console.WriteLine("Welkom bij de games library!");
             Console.WriteLine("Hierin kan je games toevoegen, games verwijderen");
             Console.WriteLine("Kies een optie\n");
+
+            // Laat menu zien
             Opties_game_list.opties();
             string user_input = Console.ReadLine();
+
+            /*
             Console.WriteLine("Wil je ook een genre toevoegen?");
             string genre_input = Console.ReadLine();
+
             if (genre_input == "ja")
             {
                 Console.WriteLine("welke genre wil je toevoegen?");
@@ -30,63 +37,77 @@ internal class Program
                 Console.ReadLine();
                 
             }
+            */
 
-            if (user_input == "1")
+            
+            switch (user_input)
             {
-                Console.WriteLine("welke game wil je toevoegen?");
-                string game = Console.ReadLine();
-                games_user.Add(game);
-                Console.WriteLine($"Oke {game} hij is toegevoegd aan de lijst!");
-                Console.WriteLine("wil je nog een game toevoegen?");
-                string choice = Console.ReadLine();
+                // Toevoegen
+                case "1":
+                    do
+                    {
+                        Console.WriteLine("welke game wil je toevoegen?");
+                        string game = Console.ReadLine();
+                        games_user.Add(game);
+                        Console.WriteLine($"Oke {game} hij is toegevoegd aan de lijst!");
+                        Console.WriteLine("wil je nog een game toevoegen?");
+                        choice = Console.ReadLine();
+                    } while (choice == "ja");
 
-                if (choice == "nee")
-                {
                     Console.WriteLine("Oke deze games zijn er toegevoegd:");
                     foreach (string game_toegevoegd in games_user)
                     {
-                        Console.WriteLine($"{game_toegevoegd}\n");
+                        Console.WriteLine($"{game_toegevoegd}");
                     }
-                }
-                else if (choice == "ja")
-                {
-                    Console.WriteLine("Oke welke game wil je nog toevoegen?");
-                    string user_input_game = Console.ReadLine();
-                    games_user.Add(user_input_game);
+                break;
+
+                // games lijst inzien
+                case "2":
+
+                    Console.WriteLine("Hier zijn de games die je hebt toegevoegd:");
+                    foreach (string game in games_user)
+                    {
+                        Console.WriteLine(game);
+                    }
                     Console.WriteLine("Druk op enter om door te gaan");
                     Console.ReadLine();
-
-                }
-            }
-            else if (user_input == "2")
-            {
-                Console.WriteLine("Hier zijn de games die je hebt toegevoegd:");
-                foreach (string game in games_user)
-                {
-                    Console.WriteLine(game);
-                }
-                Console.WriteLine("Druk op enter om door te gaan");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else if (user_input == "3")
-            {
-                int count = 0;
-                Console.WriteLine("Welke game wil je verwijderen?");
-                foreach (string game in games_user)
-                {
-                    count++;
-                    Console.WriteLine($"{count}: {game}");
-                }
-                int game_delete_user = int.Parse(Console.ReadLine()) - 1;
-                games_user.RemoveAt(game_delete_user);
-                Console.WriteLine($"Deze game is verwijderd uit je libary {games_user}");
-
-            }
-            else if (user_input == "x")
-            {
-                Console.WriteLine("Houdoe! Tot de volgende keer");
+                    Console.Clear();
                 break;
+
+                // verwijderen
+                case "3":
+
+                    int count = 0;
+                    Console.WriteLine("Welke game wil je verwijderen?");
+                    foreach (string game in games_user)
+                    {
+                        count++;
+                        Console.WriteLine($"{count}: {game}");
+                    }
+
+                    // De list index begint bij 0 
+                    // de gebruiker begint bij 1
+                    // dus daarom staat -1 erbij
+
+                    int game_delete_user = int.Parse(Console.ReadLine()) - 1;
+                    games_user.RemoveAt(game_delete_user);
+                    Console.WriteLine($"Deze game is verwijderd uit je libary.");
+                break;
+
+                // exit
+                case "x":
+
+                    Console.WriteLine("Houdoe! Tot de volgende keer");
+                    System.Environment.Exit(1);
+
+                break;
+
+
+                default:
+
+                    Console.WriteLine("ongeldige keuze");
+                    
+                    break;
             }
         }
     }
